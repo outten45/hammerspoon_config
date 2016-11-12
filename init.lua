4e03968d39dbd41ad504674f639360c8c7d9f43f
Richard Outten <outtenr@gmail.com>
2016-11-12 15:08:36 -0500

    Change to support sierra
    Karabiner with the mapping for the hyper key wasn't there anymore (at
    least not straight forward). This does the mapping in hammerspoon with
    karabiner-elements setting caps_lock to f18.

--- init.lua
+++ init.lua
@@ -3,6 +3,33 @@
 -- (Node: Capslock bound to cmd+alt+ctrl+shift via Seil and Karabiner)
 local hyper = {"⌘", "⌥", "⌃", "⇧"}
 
+-- **************************************************
+-- A hack to get the hyper key to work on sierra
+--   https://github.com/lodestone/hyper-hacks
+--   http://brettterpstra.com/2016/09/29/a-better-hyper-key-hack-for-sierra/
+-- A global variable for the Hyper Mode
+k = hs.hotkey.modal.new({}, "F17")
+
+-- Enter Hyper Mode when F18 (Hyper/Capslock) is pressed
+pressedF18 = function()
+   k.triggered = false
+   k:enter()
+end
+
+-- Leave Hyper Mode when F18 (Hyper/Capslock) is pressed,
+--   send ESCAPE if no other keys are pressed.
+releasedF18 = function()
+   k:exit()
+   if not k.triggered then
+      hs.eventtap.keyStroke({}, 'ESCAPE')
+   end
+end
+
+-- Bind the Hyper key
+f18 = hs.hotkey.bind({}, 'F18', pressedF18, releasedF18)
+-- **************************************************
+
+
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
   hs.notify.new({title="Hammerspoon", informativeText="Hello World"}):send()
 end)
@@ -35,24 +62,24 @@ end
 local safariBrowser = "Safari Technology Preview"
 
 ---------------------------------------
--- hotkey bindings for applications
-hs.hotkey.bind(hyper, "u", function() launchApp("Google Chrome") end)
-hs.hotkey.bind(hyper, "e", function() launchApp("Emacs") end)
-hs.hotkey.bind(hyper, "f", function() launchApp("Firefox") end)
-hs.hotkey.bind(hyper, "m", function() launchApp("Microsoft Outlook") end)
-hs.hotkey.bind(hyper, "i", function() launchApp("iTerm2") end)
-hs.hotkey.bind(hyper, "x", function() launchApp("Terminal", {launch = true}) end)
-hs.hotkey.bind(hyper, "o", function() launchApp("Jabber") end)
-hs.hotkey.bind(hyper, "c", function() launchApp("Opera") end)
-hs.hotkey.bind(hyper, "l", function() launchApp(safariBrowser) end)
-hs.hotkey.bind(hyper, ".", function() launchApp("Todoist", {launch = true}) end)
-hs.hotkey.bind(hyper, "y", function() launchApp("Code") end)
-hs.hotkey.bind(hyper, "8", function() launchApp("LimeChat") end)
-hs.hotkey.bind(hyper, "9", function() launchApp("TiddlyDesktop") end)
-hs.hotkey.bind(hyper, "b", function() launchApp("Atom") end)
--- hs.hotkey.bind(hyper, "p", function() launchApp("Simplenote") end)
-hs.hotkey.bind(hyper, "p", function() launchApp("Microsoft OneNote", {launch = true}) end)
-hs.hotkey.bind(hyper, "3", function() launchApp("Vivaldi") end)
+-- bindings for applications
+k:bind({}, "u", function() launchApp("Google Chrome") end)
+k:bind({}, "e", function() launchApp("Emacs") end)
+k:bind({}, "f", function() launchApp("Firefox") end)
+k:bind({}, "m", function() launchApp("Microsoft Outlook") end)
+k:bind({}, "i", function() launchApp("iTerm2") end)
+k:bind({}, "x", function() launchApp("Terminal", {launch = true}) end)
+k:bind({}, "o", function() launchApp("Jabber") end)
+k:bind({}, "c", function() launchApp("Opera") end)
+k:bind({}, "l", function() launchApp(safariBrowser) end)
+k:bind({}, ".", function() launchApp("Todoist", {launch = true}) end)
+k:bind({}, "y", function() launchApp("Code") end)
+k:bind({}, "8", function() launchApp("LimeChat") end)
+k:bind({}, "9", function() launchApp("TiddlyDesktop") end)
+k:bind({}, "b", function() launchApp("Atom") end)
+-- k:bind({}, "p", function() launchApp("Simplenote") end)
+k:bind({}, "p", function() launchApp("Microsoft OneNote", {launch = true}) end)
+k:bind({}, "3", function() launchApp("Vivaldi") end)
 
 function listAppTitles()
   hs.fnutils.each(hs.application.runningApplications(), function(app) print("[" .. app:title() .. "]") end)
@@ -87,16 +114,16 @@ function posFullHeight(ratioUsed, moveRight)
 end
 
 -- left most
-hs.hotkey.bind(hyper, "g", function() posFullHeight(0.7, false) end)
+k:bind({}, "g", function() posFullHeight(0.7, false) end)
 -- right most
-hs.hotkey.bind(hyper, "r", function() posFullHeight(0.7, true) end)
+k:bind({}, "r", function() posFullHeight(0.7, true) end)
 
 -- left half
-hs.hotkey.bind(hyper, "t", function() posFullHeight(0.5, false) end)
+k:bind({}, "t", function() posFullHeight(0.5, false) end)
 -- full screen
-hs.hotkey.bind(hyper, "n", function() posFullHeight(1.0, false) end)
+k:bind({}, "n", function() posFullHeight(1.0, false) end)
 -- right half
-hs.hotkey.bind(hyper, "s", function() posFullHeight(0.5, true) end)
+k:bind({}, "s", function() posFullHeight(0.5, true) end)
 
 -- bind right:ctrl;shift push right bar-resize:screenSizeX/3
 -- bind left:ctrl;shift  push left  bar-resize:screenSizeX/3
@@ -125,9 +152,9 @@ function mouseHighlight()
         hs.timer.doAfter(0.6, function() mouseCircle:delete() end)
     end)
 end
-hs.hotkey.bind(hyper, 'd', mouseHighlight)
+k:bind({}, 'd', mouseHighlight)
 
-hs.hotkey.bind(hyper, '1', function()
+k:bind({}, '1', function()
                  hs.alert.show(os.date("%x %X"))
                  -- local p = hs.geometry.point(100,100)
                  -- local r = hs.geometry.rect(100, 100, 300, 300)
@@ -170,7 +197,7 @@ function toggleJabberMeetingStatus()
   end
 end
 
-hs.hotkey.bind(hyper, '2', toggleJabberMeetingStatus)
+k:bind({}, '2', toggleJabberMeetingStatus)
 
 ----------------------------------------
 -- screen saver to lock screen
